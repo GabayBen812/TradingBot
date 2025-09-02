3872bc064ec826c7860ee3a899699b90acfe27bd
Ben <7ben7ben@gmail.com>
2025-09-02 09:27:16 +0300

    .

--- tradingTracker/server/src/index.js
+++ tradingTracker/server/src/index.js
@@ -27,7 +27,9 @@ app.use(cors({
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://trading-tracker.netlify.app',
+    'https://tradeapi.staysync.co.il',
   ],
   credentials: true
 }));
